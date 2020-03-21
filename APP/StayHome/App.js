1f6e4dd9ac5ea3f0bfc4aac0b007791bcd7c8f66
clibom <valentinmetz.z@gmail.com>
2020-03-21 16:21:16 +0100

    [NEW] Add ui stopwatch element

--- APP/StayHome/App.js
+++ APP/StayHome/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {Card} from 'react-native-elements';
+import {Stopwatch} from 'react-native-stopwatch-timer';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <Card title="Temps de confinement">
+        <Stopwatch laps msecs />
+      </Card>
     </View>
   );
 }
